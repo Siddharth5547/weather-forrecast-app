53180aa74e39fe600fc58582367564b6d3bb3eb7
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-12 11:03:39 +0530

    Fix backend deployment

--- server/Index.js
+++ server/Index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
-const WeatherRoutes = require("./Routes/WeatherRoutes");
+const weatherRoutes = require("./Routes/WeatherRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/weather", WeatherRoutes);
+app.use("/api/weather", weatherRoutes);
 
 module.exports = app;
