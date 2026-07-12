a43502cbcf4c112d34a695e4f95fb2a807b157b1
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-12 10:11:02 +0530

    Configure backend for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const weatherRoutes = require("./routes/weatherRoutes");
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/weather", weatherRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
