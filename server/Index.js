e690cb3556b47dcce7c90646d82970ed187e99e2
Siddharth <Siddharthbharti0135@gmail.com>
2026-07-12 14:58:14 +0530

    Fix CORS for frontend

--- server/Index.js
+++ server/Index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+app.use(
+  cors({
+    origin: [
+      "https://weather-verse-b79bu3624-weather-now.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST"],
+  })
+);
+
 const weatherRoutes = require("./Routes/WeatherRoutes");
 
 const app = express();
